789bd26f2d6d8cda9a54316d94925de7a0ca8526
Sriram P C <sriram.pc@gmail.com>
2024-12-27 11:21:12 -0500

    Completed exercise 2.19

--- part2/countries/src/components/FilterCountry.tsx
+++ part2/countries/src/components/FilterCountry.tsx
@@ -10,6 +10,13 @@ const FilterCountry = (props: FilterPropsInterface) => {
         setFilterText(target.value)
     }
 
+    const handleShowClick = (showCx: CountryInterface) => {
+        const selecteds = props.countries.filter(
+            (cx: CountryInterface) => cx.name.common === showCx.name.common
+        )
+        props.setSelectedCountries(selecteds)
+    }
+
     useEffect(() => {
         console.log(filterTxt, props.countries)
         if (filterTxt != null && filterTxt.length > 0 && props.countries.length > 0) {
@@ -28,7 +35,12 @@ const FilterCountry = (props: FilterPropsInterface) => {
         <>
             <div>
                 filter shown with <input value={filterTxt} onChange={handleFilterChange} />
-                {props.selectedCountries.map((cx: CountryInterface) => <p>{cx.name.common}</p>)}
+                {
+                    props.selectedCountries.map(
+                        (cx: CountryInterface) =>
+                            <p>{cx.name.common} <button onClick={() => handleShowClick(cx)}> Show </button></p>
+                    )
+                }
             </div>
         </>
     )
