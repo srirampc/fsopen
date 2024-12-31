035c6e95d7eb12fea40be5892fc7f58cd8b81d56
Sriram P C <sriram.pc@gmail.com>
2024-12-31 12:41:47 -0500

    Completed Exercise 4.1

---
+++ part4/blog_list/README.md
@@ -0,0 +1,5 @@
+# Exercieses 4.1 
+
+Problem Statements:
+
+- [4.1 -- 4.2](https://fullstackopen.com/en/part4/structure_of_backend_application_introduction_to_testing#exercises-4-1-4-2)
---
+++ part4/blog_list/eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+/** @type {import('eslint').Linter.Config[]} */
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+    },
+  },
+  {
+    ignores: ['dist/**'],
+  },
+]
+
