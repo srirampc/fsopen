323237d2da424cf83164a004da1e7acd96468e4c
Sriram P C <sriram.pc@gmail.com>
2025-01-28 08:48:25 -0500

    Initial version for 7.1 exercies ported from the javascript version

---
+++ part7/anecdotes_rtr/README.md
@@ -0,0 +1,5 @@
+# Solution to Part 7
+
+Problem statements for the solutions are below:
+
+- [7.1 -- 7.3](https://fullstackopen.com/en/part7/react_router#exercises-7-1-7-3)
---
+++ part7/anecdotes_rtr/src/ifx.ts
@@ -0,0 +1,15 @@
+export interface IAnecdote {
+  content: string
+  author: string
+  info: string
+  id?: number
+  votes: number
+}
+
+export interface IAListProps {
+    anecdotes: IAnecdote[]
+}
+
+export interface ICreateProps {
+    addNew: (anew: IAnecdote) => void
+}
